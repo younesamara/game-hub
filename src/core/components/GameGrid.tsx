65bd78104b337dc8f7f2fb48dd04b02a2ce23656
younes amara <younes.amara@etu.umontpellier.fr>
2024-08-01 20:11:10 +0200

    Clean up game cards

--- src/core/components/GameGrid.tsx
+++ src/core/components/GameGrid.tsx
@@ -21,7 +21,7 @@ export function GameGrid({gameQuery}: Props) {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} padding='10px' spacing={3}>
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl:4}} padding='10px' spacing={6}>
                 {isLoading && skeletons.map((skeleton) =>
                     <GameCardContainer key={skeleton}>
                         <GameCardSkeleton/>
