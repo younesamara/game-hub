8feb7929ba901d37f74e558ce0e46bbc6582673b
younes amara <younes.amara@etu.umontpellier.fr>
2024-08-01 19:39:34 +0200

    Fix issue with chakra menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {Grid, GridItem, HStack, Show} from "@chakra-ui/react";
+import {Box, Flex, Grid, GridItem, Show} from "@chakra-ui/react";
 import {NavBar} from "@core";
 import {GameGrid} from "./core";
 import GenreList from "./core/components/GenreList.tsx";
 import {useState} from "react";
 import {Genre} from "./hooks/useGenres.ts";
 import PlatformSelector from "./core/components/PlatformSelector.tsx";
-import {Game, Platform} from "./hooks/useGames.ts";
+import {Platform} from "./hooks/useGames.ts";
 import SortSelector from "./core/components/SortSelector.tsx";
 
 
@@ -39,13 +39,15 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area="main">
-                <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-                    <PlatformSelector selectedPlatform={gameQuery.platform}
-                                      onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
-                    <SortSelector sortOrder={gameQuery.sortOrder}/
-                        onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+                <Flex paddingLeft={2} marginBottom={5}>
+                    <Box marginRight={5}>
+                        <PlatformSelector selectedPlatform={gameQuery.platform}
+                                          onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+                    </Box>
+                    <SortSelector sortOrder={gameQuery.sortOrder}
+                                  onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
 
-                </HStack>
+                </Flex>
                 <GameGrid gameQuery={gameQuery}/>
             </GridItem>
 
